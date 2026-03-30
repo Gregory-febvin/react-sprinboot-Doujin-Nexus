28f87b538acafb38a8f73992df129c1908ef6033
Grégory Febvin <greg.feb20@gmail.com>
2026-03-30 16:10:37 +0200

    Page d'acceuille avec image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 import Layout from './component/Layout';
 import Home from './page/Home';
-//import Sauce from './pages/Sauce';
+import Sauce from './page/Sauce';
 //import ImageFrame from './components/ImageFrame';
 //import Browsing from './components/Browsing';
 
@@ -16,8 +16,8 @@ function App() {
     <Routes>
       <Route path={ROUTES.HOME} element={<Layout />}>
         <Route index element={<Home />} />
-        {/* <Route path={ROUTES.SAUCE} element={<Sauce />} />
-        <Route path={ROUTES.SAUCE_THUMBNAIL} element={<ImageFrame />} />
+        <Route path={ROUTES.SAUCE} element={<Sauce />} />
+        {/* <Route path={ROUTES.SAUCE_THUMBNAIL} element={<ImageFrame />} />
         <Route path={ROUTES.SAUCE_NAME} element={<Browsing />} /> */}
       </Route>
     </Routes>
--- frontend/src/page/Home.tsx
+++ frontend/src/page/Home.tsx
@@ -1,23 +1,27 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Home() {
   const [items, setItems] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch('http://127.0.0.1:5000/api/manga')
-      .then(res => res.json())
-      .then(data => setItems(data))
+    axios.get('http://127.0.0.1:8080/api/manga')
+      .then(res => setItems(res.data))
       .catch(err => console.error('Erreur:', err));
   }, []);
 
   return (
-    <div className="gallery">
+    <div className="flex flex-wrap justify-center gap-5 w-4/5 mx-auto mt-[1%]">
       {items.map(item => (
-        <div key={item.id} className="card" onClick={() => navigate(`/sauce/${item.id}`)}>
-          <img src={item.cover} alt={`Cover ${item.id}`} />
-          <div className="title">{item.title}</div>
+        <div key={item.id} 
+          className="w-52 flex flex-col items-center bg-gray-800 rounded-lg overflow-hidden shadow-lg hover:shadow-xl hover:-translate-y-1 transition-transform duration-200 cursor-pointer"
+          onClick={() => navigate(`/sauce/${item.id}`)}>
+          <img src={item.cover} alt={`Cover ${item.id}`} className="w-52 h-80 object-cover block"/>
+          <div className="p-2.5 font-bold text-sm text-center text-[16px] overflow-hidden">
+            {item.title}
+          </div>
         </div>
       ))}
     </div>
